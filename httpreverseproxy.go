9cb8c57fdf950923ed6c17497f949f77f5040b98
Mr Amini <h_amini@outlook.com>
2023-02-21 16:01:00 +0330

    add comments

--- httpreverseproxy.go
+++ httpreverseproxy.go
@@ -12,27 +12,36 @@ import (
 )
 
 func StartHTTPReverseProxyServer(done chan int) {
+	// Get the proxy server host and port from the PROXY_HOST environment variable.
 	proxyHost := os.Getenv("PROXY_HOST")
 
 	if proxyHost == "" {
+		// If PROXY_HOST is not set, use the default proxy server.
 		proxyHost = "127.0.0.1:1089"
 	}
 
+	// Create a Dialer with a timeout and keep-alive period of 30 seconds.
 	baseDialer := net.Dialer{
 		Timeout:   30 * time.Second,
 		KeepAlive: 30 * time.Second,
 	}
 
+	// Create a SOCKS5 dialer using the proxy server host and port, and the base dialer.
 	dialSocksProxy, err := proxy.SOCKS5("tcp", proxyHost, nil, &baseDialer)
 	if err != nil {
+		// If there's an error creating the dialer, log it and return.
 		log.Print(err)
 		return
 	}
 
+	// Convert the SOCKS5 dialer to a ContextDialer.
 	contextDialer := dialSocksProxy.(proxy.ContextDialer)
 
+	// Define an HTTP handler function that sets up a reverse proxy for incoming requests.
 	http.HandleFunc("/", func(w http.ResponseWriter, target *http.Request) {
+		// Log the details of incoming requests.
 		log.Printf("HTTP Reverse Proxy = host:%s, requrl:%s, method:%s\n", target.Host, target.RequestURI, target.Method)
+		// Create a reverse proxy using the SOCKS5 dialer and the Director function to set the scheme and host of outgoing requests.
 		reverseProxy := &httputil.ReverseProxy{
 			Transport: &http.Transport{
 				Proxy:                 http.ProxyFromEnvironment,
@@ -48,11 +57,15 @@ func StartHTTPReverseProxyServer(done chan int) {
 			},
 		}
 
+		// Serve the incoming request using the reverse proxy.
 		reverseProxy.ServeHTTP(w, target)
 	})
+
+	// Start the HTTP server to listen on port 80 and serve incoming requests.
 	log.Println("Listening on port :80 ...")
 	err = http.ListenAndServe(":80", nil)
 	if err != nil {
+		// If there's an error starting the server, panic.
 		panic(err)
 	}
 }
